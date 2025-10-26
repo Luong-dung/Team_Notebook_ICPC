646b8f79b4885a49673b34882ba71ee23b0c0ef3
Luong-dung <luongdung90050101@gmail.com>
2025-10-27 01:14:20 +0700

    Initial commit

---
+++ 00_base_template/base.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ar array
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+void fast_io() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+// Binary Search Snippet (Standard Lower Bound)
+// Finds the first element >= x
+int bin_search_lower(const vector<int>& arr, int x) {
+    int l = 0, r = arr.size() - 1;
+    int ans = -1;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] >= x) {
+            ans = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
+
+// Binary Search on Answer Snippet
+// Finds the maximum value 'ans' such that check(ans) is true
+bool check(int val) {
+    // Check function logic here
+    return true; 
+}
+
+int bin_search_answer() {
+    int l = 0, r = 1e9; 
+    int ans = 0;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (check(mid)) {
+            ans = mid;
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    return ans;
+}
+
+
+void solve() {
+    // Your code here
+}
+
+int main() {
+    fast_io();
+    // int tc = 1;
+    // cin >> tc;
+    // for (int t = 1; t <= tc; t++) {
+        solve();
+    // }
+    return 0;
+}
