6aa8286bb7d9fbf10ad67df72c7d2ae7c00b9194
Luong-dung <luongdung90050101@gmail.com>
2025-11-07 22:37:51 +0700

    Update math.cpp

--- 01_MATH_NUMBER_THEORY/math.cpp
+++ 01_MATH_NUMBER_THEORY/math.cpp
@@ -67,21 +67,18 @@ struct Math {
         return x;
     }
     // ---------- Miller Rabin --------------
-    bool test(long long a, long long n, long long k, long long m)
-    {
+    bool test(long long a, long long n, long long k, long long m){
         long long mod = binaryPower(a, m, n); // a^m % n
         if (mod == 1 || mod == n - 1)
                 return true;
-        for (int l = 1; l < k; ++l)
-        {
+        for (int l = 1; l < k; ++l){
             mod = (mod * mod) % n;
             if (mod == n - 1)
                 return true;
         }
         return false;
     }
-    bool MillerRabin(long long n)
-    {
+    bool MillerRabin(long long n){
         static vector<int> checkSet = {2,3,5,7,11,13,17,19,23,29,31,37};
         for (auto a : checkSet)
             if (n == a)
@@ -90,8 +87,7 @@ struct Math {
             return false;
 
         long long k = 0, m = n - 1;
-        while (m % 2 == 0)
-        {
+        while (m % 2 == 0){
             m /= 2;
             k++;
         }
